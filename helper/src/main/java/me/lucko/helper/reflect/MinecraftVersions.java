45fa6a411980f52f3b27aa98a6153ea14281cc39
Teije <iteije@outlook.com>
2024-11-07 14:34:15 +0100

    Add 1.21.3 support

--- helper/src/main/java/me/lucko/helper/reflect/MinecraftVersions.java
+++ helper/src/main/java/me/lucko/helper/reflect/MinecraftVersions.java
@@ -38,6 +38,10 @@ import java.util.regex.Pattern;
 public final class MinecraftVersions {
 
     /**
+     * Version 1.21 - the tricky trials update
+     */
+    public static final MinecraftVersion v1_21 = MinecraftVersion.parse("1.21");
+    /**
      * Version 1.20 - the trails and tails update
      */
     public static final MinecraftVersion v1_20 = MinecraftVersion.parse("1.20");
