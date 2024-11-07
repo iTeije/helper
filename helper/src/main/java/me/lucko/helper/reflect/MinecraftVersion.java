0df14ebf8cb925247be6d54770c297f939a83e65
Teije <iteije@outlook.com>
2024-11-07 14:34:15 +0100

    Add 1.21.3 support

--- helper/src/main/java/me/lucko/helper/reflect/MinecraftVersion.java
+++ helper/src/main/java/me/lucko/helper/reflect/MinecraftVersion.java
@@ -50,12 +50,12 @@ public final class MinecraftVersion implements Comparable<MinecraftVersion> {
     /**
      * The newest known version of Minecraft
      */
-    private static final String NEWEST_MINECRAFT_VERSION = "1.20.1";
+    private static final String NEWEST_MINECRAFT_VERSION = "1.21.3";
 
     /**
      * The date (with ISO 8601 or YYYY-MM-DD) when the most recent version was released.
      */
-    private static final String MINECRAFT_LAST_RELEASE_DATE = "2023-06-07";
+    private static final String MINECRAFT_LAST_RELEASE_DATE = "2024-10-23";
 
     /**
      * Gets the {@link MinecraftVersion} of the runtime server.
--- helper/src/main/java/me/lucko/helper/reflect/NmsVersion.java
+++ helper/src/main/java/me/lucko/helper/reflect/NmsVersion.java
@@ -125,6 +125,29 @@ public enum NmsVersion {
             MinecraftVersion.of(1, 19, 0),
             MinecraftVersion.of(1, 19, 1),
             MinecraftVersion.of(1, 19, 2)
+    ),
+    v1_20_R1(
+            MinecraftVersion.of(1, 20, 0),
+            MinecraftVersion.of(1, 20, 1)
+    ),
+    v1_20_R2(
+            MinecraftVersion.of(1, 20, 2)
+    ),
+    v1_20_R3(
+            MinecraftVersion.of(1, 20, 3),
+            MinecraftVersion.of(1, 20, 4)
+    ),
+    v1_20_R4(
+            MinecraftVersion.of(1, 20, 5),
+            MinecraftVersion.of(1, 20, 6)
+    ),
+    v1_21_R1(
+            MinecraftVersion.of(1, 21, 0),
+            MinecraftVersion.of(1, 21, 1)
+    ),
+    v1_21_R2(
+            MinecraftVersion.of(1, 21, 2),
+            MinecraftVersion.of(1, 21, 3)
     );
 
     private final Set<MinecraftVersion> minecraftVersions;
